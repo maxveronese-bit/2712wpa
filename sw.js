85d375a31069a654af0fb1e3eb915716d1204427
maxveronese-bit <max.veronese@veroneselaw.com>
2026-02-04 14:32:47 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,19 @@
-const CACHE_NAME = '2712wpa';
-const FILES = ['/', '/index.html', '/app.js', '/manifest.json'];
+const CACHE_NAME = 'personalos-v5.4';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => caches.delete(k))
+    ))
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
